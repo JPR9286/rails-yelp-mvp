42466addd1c2238a7211e2b4db45777288d7eae2
Jerome PRINCET <jerome@princet.fr>
2023-11-15 16:21:05 +0100

    Exercie Terminé !

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,11 +7,15 @@ class RestaurantsController < ApplicationController
   end
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save
-    redirect_to restaurant_path(@restaurant)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
   def show
-
+    @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   private
@@ -22,12 +26,12 @@ end
 
 
 # Un visiteur peut voir la liste de tous les restaurants.
-# GET "restaurants"
+# GET "restaurants" DONE
 # Un visiteur peut ajouter un nouveau restaurant et être redirigé vers la vue show de la page de ce nouveau restaurant.
-# GET "restaurants/new"
-# POST "restaurants"
+# GET "restaurants/new" DONE
+# POST "restaurants" DONE
 # Un visiteur peut voir les détails d’un restaurant, avec tous les avis associés au restaurant.
-# GET "restaurants/38"
+# GET "restaurants/38" DONE
 # Un visiteur peut ajouter un nouvel avis à un restaurant.
-# GET "restaurants/38/reviews/new"
+# GET "restaurants/38/reviews/new" DONE
 # POST "restaurants/38/reviews"
